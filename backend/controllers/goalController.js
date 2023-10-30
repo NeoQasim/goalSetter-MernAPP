fc53845b2512f1aee02400bdbc24490f0a9ec663
neoqasim <neoqasim@gmail.com>
2023-10-30 18:34:37 +0500

    goal controller and routes Setup

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,27 +1,27 @@
+const asyncHandler = require('express-async-handler');
 
-
-const getGoals = (req, res) => { //@desc Get the Goals ,   @route /api/goals,  @method post  @access Private
+const getGoals = asyncHandler(async (req, res) => { //@desc Get the Goals ,   @route /api/goals,  @method post  @access Private
     res.status(200).json({
         messagess: "get goal "
     })
-}
-const addGoal = (req, res) => { //@desc Get the Goals ,   @route /api/goals,  @method post  @access Private
+})
+const addGoal = asyncHandler(async (req, res) => { //@desc Get the Goals ,   @route /api/goals,  @method post  @access Private
     if (!req.body.text) {
         res.status(400)
         throw new Error("please enter the text field")
     }
     res.status(400).json({ messagess: "this text is added from body" })
-}
-const updateGoals = (req, res) => { //@desc Get the Goals ,   @route /api/goals/:id,  @method put  @access Private
+})
+const updateGoals = asyncHandler(async (req, res) => { //@desc Get the Goals ,   @route /api/goals/:id,  @method put  @access Private
     res.status(200).json({
         messagess: ` updated the goal with id ${req.params.id} `
     })
-}
-const deleteGoals = (req, res) => { //@desc Get the Goals ,   @route /api/goals,  @method delete  @access Private
+})
+const deleteGoals = asyncHandler(async (req, res) => { //@desc Get the Goals ,   @route /api/goals,  @method delete  @access Private
     res.status(200).json({
         messagess: ` deleted the goal with id ${req.params.id} `
     })
 }
-
+)
 
 module.exports = { getGoals, addGoal, updateGoals, deleteGoals }
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { errorHandler } = require('./middleware/errorMiddleware');
 const dotenv = require("dotenv").config()
 const port = process.env.port
 require('colors');
@@ -7,8 +8,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use("/api/goals", require("./routes/goalRoutes"))
-
-
-
+app.use(errorHandler)
 app.listen(port, () => { console.log(`server started on ${port}`) })
 console.log("nodemomn server working");
